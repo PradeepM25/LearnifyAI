26df99c0d2ee355ba94883e17077a464fe9d004f
Pradeep <machapradeep478@gmail.com>
2025-09-14 14:37:53 +0530

    modified authAPI.js baseurl

--- frontend/src/features/auth/authApi.js
+++ frontend/src/features/auth/authApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const authApi = createApi({
   reducerPath: "authApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:4000/api" }), // adjust backend URL
+  baseQuery: fetchBaseQuery({ baseUrl: `${import.meta.env.VITE_BACKEND_URL}/api` }), // adjust backend URL
   endpoints: (builder) => ({
     login: builder.mutation({
       query: (credentials) => ({
